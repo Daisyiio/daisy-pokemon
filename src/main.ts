8f27f17601b30cd7aaebc6654824cb75ddebc7d3
Daisy <554091877@qq.com>
2025-01-19 16:42:25 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 import * as ElIcons from '@element-plus/icons-vue';
 
-const title = 'Plan Random'
+const title = 'Daisy Pokemon'
 const app = createApp(App)
 app.use(router)
 app.use(ElementPlus)
